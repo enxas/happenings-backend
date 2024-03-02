4807b748a51a2bbc150b046564756a661f9f4c71
Edvinas <8630984+enxas@users.noreply.github.com>
2024-03-02 16:43:25 +0200

    x

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
 	jwtAlgorithm: "HS256",
-	accessTokenExpiration: "10h",
+	accessTokenExpiration: "12h",
 	refreshTokenExpiration: "1d",
 	refreshTokenExpirationMs: 24 * 60 * 60 * 1000
 }
